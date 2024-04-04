ef269a78e755b2f37493c42e709ae99b1177368d
Vazz2004 <samivazqueles@gmail.com>
2024-04-04 18:25:16 -0500

    se graga la vista de home

--- src/view/HomeScreen.tsx
+++ src/view/HomeScreen.tsx
@@ -1,29 +1,18 @@
-import React, { useState } from 'react';
-import { View, StyleSheet, ScrollView, TouchableOpacity, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, StyleSheet, ScrollView, Text, Alert } from 'react-native';
 import PetCard from '../components/PetCard';
-
-interface Pet {
-  name: string;
-  species: string;
-  breed: string;
-  age: number;
-  color: string;
-  photo: string;
-}
+import { LocalStorage } from '../utils/LocalStorage';
+import MascotaViewModel from '../viewModels/MascotasViewModel';
 
 export default function Home() {
-  const [pets, setPets] = useState<Pet[]>([
-    { name: "Max", species: "Perro", breed: "Golden Retriever", age: 5, color: "Dorado", photo: "https://img.freepik.com/foto-gratis/lindo-mascota-collage-aislado_23-2150007407.jpg" },
-    { name: "Luna", species: "Gato", breed: "Siamés", age: 3, color: "Blanco", photo: "https://img.freepik.com/foto-gratis/lindo-mascota-collage-aislado_23-2150007407.jpg" }
-  ]);
-
+  const { values } = MascotaViewModel();
   const handleVaccinePress = () => {
-    console.log('Botón "Ver Vacunas" presionado');
+    console.log('lógica de vacuna');
   };
 
-  const handleSettingsPress = () => {
-    console.log('Botón "Configuración" presionado');
-  };
+  const handleHospitalizationCertificatesPress = () => {
+    console.log('boton hospitalizaciones')
+  }
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -31,32 +20,18 @@ export default function Home() {
         <Text style={styles.subtitle}>¡Cuida a tus mascotas como se merecen!</Text>
       </View>
       <ScrollView contentContainerStyle={styles.scrollContainer}>
-        <View style={styles.section}>
-          <Text style={styles.sectionTitle}>Perros</Text>
-          {pets.filter(pet => pet.species === 'Perro').map((pet, index) => (
-            <PetCard
-              key={index}
-              name={pet.name}
-              species={pet.species}
-              age={pet.age}
-              photo={pet.photo}
-              onPressVaccines={handleVaccinePress}
-            />
-          ))}
-        </View>
-        <View style={styles.section}>
-          <Text style={styles.sectionTitle}>Gatos</Text>
-          {pets.filter(pet => pet.species === 'Gato').map((pet, index) => (
-            <PetCard
-              key={index}
-              name={pet.name}
-              species={pet.species}
-              age={pet.age}
-              photo={pet.photo}
-              onPressVaccines={handleVaccinePress}
-            />
-          ))}
-        </View>
+      {values.map((pet: { nombre_mascota: string; raza_mascota: string }, index: number) => (
+  <View style={styles.section} key={index}>
+    <PetCard
+      name={pet.nombre_mascota}
+      species={pet.raza_mascota}
+      photo='https://img.freepik.com/foto-gratis/lindo-mascota-collage-aislado_23-2150007407.jpg'
+      onPressVaccines={handleVaccinePress}
+      onPressHospitalizationCertificates={handleHospitalizationCertificatesPress} // Agrega esta línea
+    />
+  </View>
+))}
+
       </ScrollView>
     </View>
   );
