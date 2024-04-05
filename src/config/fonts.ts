bbe0635559b174596acc86e37a5b80e7fa2e34d0
DD150602 <ljmdylan.delgado1011@gmail.com>
2024-04-05 11:18:13 -0500

    🐛 Se comentan fuentes devido a errores

--- src/config/fonts.ts
+++ src/config/fonts.ts
@@ -1,9 +1,9 @@
 import * as Font from "expo-font";
 
 const fontMap = {
-  "poppins-regular": require("../../assets/fonts/Poppins-Bold.ttf"),
-  "poppins-bold": require("../../assets/fonts/Poppins-SemiBold.ttf"),
-  "poppins-semiBold": require("../../assets/fonts/Poppins-Regular.ttf"),
+  // "poppins-bold": require("../../assets/fonts/Poppins-Bold.ttf"),
+  // "poppins-semiBold": require("../../assets/fonts/Poppins-SemiBold.ttf"),
+  "poppins-regular": require("../../assets/fonts/Poppins-Regular.ttf"),
 };
 
 const loadFonts = async () => {
--- src/utils/Fonst.ts
+++ src/utils/Fonst.ts
@@ -1,5 +1,5 @@
 export default {
   "poppins-regular": "poppins-regular",
-  "poppins-bold": "poppins-bold",
-  "poppins-semiBold": "poppins-semiBold",
+  // "poppins-bold": "poppins-bold",
+  // "poppins-semiBold": "poppins-semiBold",
 };
